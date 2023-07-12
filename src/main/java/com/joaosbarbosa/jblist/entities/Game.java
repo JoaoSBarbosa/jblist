744f93e4bff58e0dab3cae2ba6028ac69ce84a16
João Barbosa <contato.jsbarbosa@gmail.com>
2023-07-12 18:12:42 -0300

    Mapeamento

--- src/main/java/com/joaosbarbosa/jblist/entities/Game.java
+++ src/main/java/com/joaosbarbosa/jblist/entities/Game.java
@@ -2,13 +2,24 @@ package com.joaosbarbosa.jblist.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_game")
 public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title, genre,platform,imgUrl,shortDescription,longDescription;
+	@Column(name = "game_year")
 	private Integer year;
 	private Double score;
 	
-	// 
 	public Game() {
 		
 	}
