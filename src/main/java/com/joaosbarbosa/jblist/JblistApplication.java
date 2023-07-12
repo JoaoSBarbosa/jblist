5d80419c3d648c00010321fc93bb859809dd9827
João Barbosa <contato.jsbarbosa@gmail.com>
2023-07-12 14:43:28 -0300

    Project created

---
+++ src/main/java/com/joaosbarbosa/jblist/JblistApplication.java
@@ -0,0 +1,13 @@
+package com.joaosbarbosa.jblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblistApplication.class, args);
+	}
+
+}
