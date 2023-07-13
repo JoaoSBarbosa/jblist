d20c3d67ca63a786176a0e35de8b3f569537c05a
João Barbosa <contato.jsbarbosa@gmail.com>
2023-07-13 20:38:03 -0300

    GET Game by id

--- src/main/java/com/joaosbarbosa/jblist/controllers/GameController.java
+++ src/main/java/com/joaosbarbosa/jblist/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joaosbarbosa.jblist.dto.GameDTO;
 import com.joaosbarbosa.jblist.dto.GameMinDTO;
-import com.joaosbarbosa.jblist.entities.Game;
 import com.joaosbarbosa.jblist.services.GameService;
 
 @RestController
@@ -17,9 +18,16 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
+	
 }
