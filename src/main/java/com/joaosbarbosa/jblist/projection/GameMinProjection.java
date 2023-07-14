4b3e65da66690cc59c012e89d1c0460ea8bf173d
João Barbosa <contato.jsbarbosa@gmail.com>
2023-07-14 14:08:09 -0300

    Homolog ok

--- src/main/java/com/joaosbarbosa/jblist/projection/GameMinProjection.java
+++ src/main/java/com/joaosbarbosa/jblist/projection/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.joaosbarbosa.jblist.projection;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getgameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
